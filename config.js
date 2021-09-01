484e832de4cdf084b33267fd328ca90925d1a619
Tiw4Coder <70450941+Tiw4Coder@users.noreply.github.com>
2021-08-31 20:07:06 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCrOumTri3OIezQgETde0wnKBT264Tjmgg",
+  authDomain: "team-voting-92071.firebaseapp.com",
+  databaseURL: "https://team-voting-92071-default-rtdb.firebaseio.com",
+  projectId: "team-voting-92071",
+  storageBucket: "team-voting-92071.appspot.com",
+  messagingSenderId: "557678276182",
+  appId: "1:557678276182:web:ce8bb2e700a86e189755f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
